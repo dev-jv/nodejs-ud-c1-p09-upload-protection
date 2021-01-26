b98bf8e57efbb050cc92a22eef1935442f8b558e
dev-jv <dev.javierleon@gmail.com>
2021-01-26 17:20:36 -0500

    two

--- server/server.js
+++ server/server.js
@@ -1,8 +1,37 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-    res.send(':: I´m here..')
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use (bodyParser.json());
+
+// app.get('/', (req, res) => {
+//     res.send(':: I´m here..')
+// });
+
+app.get('/usuario', (req, res) => {
+    res.json('get Usuario');
+});
+
+app.post('/usuario', (req, res) => {
+    let body = req.body;
+
+    res.json({
+        body
+    });
+});
+
+app.put('/usuario/:id', (req, res) => {
+    // res.json('put Usuario');
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', (req, res) => {
+    res.json('delete Usuario');
 });
 
 app.listen(3000, () => {
