eae432d5d5affca76116c4e6c528300db1274ec2
dev-jv <dev.javierleon@gmail.com>
2021-01-28 14:44:22 -0500

    seven

--- server/server.js
+++ server/server.js
@@ -3,41 +3,13 @@ require('./config/config');
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use (bodyParser.json());
 
-app.get('/usuario', (req, res) => {
-    res.json('get Usuario');
-});
-
-app.post('/usuario', (req, res) => {
-    let body = req.body;
-
-    if( body.nombre === undefined ) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-    } else {
-        res.json({
-            body
-        });
-    }
-});
-
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', (req, res) => {
-    res.json('delete Usuario');
-});
+app.use( require('./routes/user') );
 
 mongoose.connect('mongodb://localhost:27017/cafe', {
     useNewUrlParser: true,
